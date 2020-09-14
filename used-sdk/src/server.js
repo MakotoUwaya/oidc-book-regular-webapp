742cfc27b20cbcad8fcd4441263543a1aea085f3
Makoto Uwaya <makoto.uwaya@e-seikatsu.co.jp>
2020-09-14 09:34:39 +0900

    Using passport-auth0 sdk

--- used-sdk/src/server.js
+++ used-sdk/src/server.js
@@ -4,19 +4,32 @@ const crypto = require('crypto');
 const express = require('express');
 const handlebars = require('express-handlebars');
 const path = require('path');
-const jwt = require('jsonwebtoken');
-const jwksClient = require('jwks-rsa');
-const request = require('request-promise');
+const passport = require('passport');
+const Auth0Strategy = require('passport-auth0');
 const session = require('express-session');
 
 // loading env vars from .env file
 require('dotenv').config();
 
-const nonceCookie = 'auth0rization-nonce';
-let oidcProviderInfo;
-
 const app = express();
+const auth0Strategy = new Auth0Strategy(
+  {
+    domain: process.env.OIDC_PROVIDER,
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: 'http://localhost:3001/callback'
+  },
+  (accessToken, refreshToken, extraParams, profile, done) => {
+    profile.idToken = extraParams.id_token;
+    return done(null, profile);
+  }
+);
+passport.use(auth0Strategy);
 
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((user, done) => done(null, user));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser(crypto.randomBytes(16).toString('hex')));
 app.use(
@@ -35,105 +48,32 @@ app.get('/', (req, res) => {
 });
 
 app.get('/profile', (req, res) => {
-  const { idToken, decodedIdToken } = req.session;
+  const { user } = req.session.passport;
   res.render('profile', {
-    idToken,
-    decodedIdToken
+    idToken: user.idToken,
+    decodedIdToken: user._json
   });
 });
 
-app.get('/login', (req, res) => {
-  const authorizationEndpoint = oidcProviderInfo['authorization_endpoint'];
-  const responseType = 'id_token';
-  const scope = 'openid profile email';
-  const clientID = process.env.CLIENT_ID;
-  const redirectUri = 'http://localhost:3000/callback';
-  const responseMode = 'form_post';
-  const nonce = crypto.randomBytes(16).toString('hex');
-
-  const options = {
-    maxAge: 1000 * 60 * 15,
-    httpOnly: true,
-    signed: true
-  };
-
-  // TODO: state 含めてないけどCSRF対策大丈夫？
-  res
-    .cookie(nonceCookie, nonce, options)
-    .redirect(
-      authorizationEndpoint +
-        '?response_mode=' +
-        responseMode +
-        '&response_type=' +
-        responseType +
-        '&scope=' +
-        scope +
-        '&client_id=' +
-        clientID +
-        '&redirect_uri=' +
-        redirectUri +
-        '&nonce=' +
-        nonce
-    );
-});
-
-app.post('/callback', async (req, res) => {
-  const nonce = req.signedCookies[nonceCookie];
-  delete req.signedCookies[nonceCookie];
-
-  const { id_token } = req.body;
-  const decodeToken = jwt.decode(id_token, { complete: true });
-  const kid = decodeToken.header.kid;
-  const client = jwksClient({
-    jwksUri: oidcProviderInfo['jwks_uri']
-  });
-  client.getSigningKey(kid, (err, key) => {
-    const sigingKey = key.publicKey || key.rsaPublicKey;
-    const verifiedToken = jwt.verify(id_token, sigingKey);
-    const {
-      nonce: decodedNonce,
-      aud: audience,
-      exp: expirationDate,
-      iss: issuer
-    } = verifiedToken;
-    const currentTime = Math.floor(Date.now() / 1000);
-    const expectedAudience = process.env.CLIENT_ID;
-
-    // TODO: stateの検証やってない
-    if (
-      audience !== expectedAudience ||
-      decodedNonce !== nonce ||
-      expirationDate < currentTime ||
-      issuer !== oidcProviderInfo['issuer']
-    ) {
-      return res.status(401).send();
-    }
-
-    req.session.decodedIdToken = verifiedToken;
-    req.session.idToken = id_token;
-    res.redirect('./profile');
-  });
-});
-
-app.get('/to-dos', async (req, res) => {
-  res.status(501).send();
-});
-
-app.get('/remove-to-do/:id', async (req, res) => {
-  res.status(501).send();
-});
-
-const { OIDC_PROVIDER } = process.env;
-const discEnd = `https://${OIDC_PROVIDER}/.well-known/openid-configuration`;
-request(discEnd)
-  .then(res => {
-    oidcProviderInfo = JSON.parse(res);
-    app.listen(3000, () => {
-      console.log(`Server running on http://localhost:3000`);
-    });
+app.get(
+  '/login',
+  passport.authenticate('auth0', {
+    scope: 'openid email profile'
   })
-  .catch(err => {
-    console.error(err);
-    console.error(`Unable to get OIDC endpoints for ${OIDC_PROVIDER}`);
-    process.exit(1);
-  });
+);
+
+app.get('/callback', (req, res, next) => {
+  passport.authenticate('auth0', (err, user) => {
+    if (err) return next(err);
+    if (!user) return res.redirect('/login');
+
+    req.logIn(user, (err) => {
+      if (err) return next(err);
+      res.redirect('/profile');
+    });
+  })(req, res, next);
+});
+
+app.listen(3001, () => {
+  console.log(`Server running on http://localhost:3001`);
+});
