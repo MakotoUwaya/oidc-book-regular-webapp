4003f1039726e3735c66b6c18be35061bf5ddd88
Makoto Uwaya <makoto.uwaya@e-seikatsu.co.jp>
2020-09-21 13:31:07 +0900

    Authorization to api for unused-sdk

--- unused-sdk/src/server.js
+++ unused-sdk/src/server.js
@@ -23,7 +23,7 @@ app.use(
   session({
     secret: crypto.randomBytes(32).toString('hex'),
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
   })
 );
 app.engine('handlebars', handlebars());
@@ -38,23 +38,24 @@ app.get('/profile', (req, res) => {
   const { idToken, decodedIdToken } = req.session;
   res.render('profile', {
     idToken,
-    decodedIdToken
+    decodedIdToken,
   });
 });
 
 app.get('/login', (req, res) => {
   const authorizationEndpoint = oidcProviderInfo['authorization_endpoint'];
-  const responseType = 'id_token';
-  const scope = 'openid profile email';
+  const responseType = 'code';
+  const scope = 'openid profile email read:to-dos';
   const clientID = process.env.CLIENT_ID;
   const redirectUri = 'http://localhost:3000/callback';
-  const responseMode = 'form_post';
+  const responseMode = 'query';
   const nonce = crypto.randomBytes(16).toString('hex');
+  const audience = process.env.API_IDENTIFIER;
 
   const options = {
     maxAge: 1000 * 60 * 15,
     httpOnly: true,
-    signed: true
+    signed: true,
   };
 
   // TODO: state 含めてないけどCSRF対策大丈夫？
@@ -73,50 +74,83 @@ app.get('/login', (req, res) => {
         '&redirect_uri=' +
         redirectUri +
         '&nonce=' +
-        nonce
+        nonce +
+        '&audience=' +
+        audience
     );
 });
 
-app.post('/callback', async (req, res) => {
+function validateIDToken(idToken, nonce) {
+  const decodedToken = jwt.decode(idToken);
+  const {
+    nonce: decodedNonce,
+    aud: audience,
+    exp: expirationDate,
+    iss: issuer,
+  } = decodedToken;
+  const currentTime = Math.floor(Date.now() / 1000);
+  const expectedAudience = process.env.CLIENT_ID;
+
+  if (
+    audience !== expectedAudience ||
+    decodedNonce !== nonce ||
+    expirationDate < currentTime ||
+    issuer !== oidcProviderInfo['issuer']
+  ) {
+    throw Error();
+  }
+
+  return decodedToken;
+}
+
+app.get('/callback', async (req, res) => {
+  const { code } = req.query;
+
+  const codeExchangeOptions = {
+    grant_type: 'authorization_code',
+    client_id: process.env.CLIENT_ID,
+    client_secret: process.env.CLIENT_SECRET,
+    code: code,
+    redirect_uri: 'http://localhost:3000/callback',
+  };
+
+  const codeExchangeResponse = await request.post(
+    `https://${process.env.OIDC_PROVIDER}/oauth/token`,
+    { form: codeExchangeOptions }
+  );
+
+  // parse response to get tokens
+  const tokens = JSON.parse(codeExchangeResponse);
+  req.session.accessToken = tokens.access_token;
+
+  // extract nonce from cookie
   const nonce = req.signedCookies[nonceCookie];
   delete req.signedCookies[nonceCookie];
 
-  const { id_token } = req.body;
-  const decodeToken = jwt.decode(id_token, { complete: true });
-  const kid = decodeToken.header.kid;
-  const client = jwksClient({
-    jwksUri: oidcProviderInfo['jwks_uri']
-  });
-  client.getSigningKey(kid, (err, key) => {
-    const sigingKey = key.publicKey || key.rsaPublicKey;
-    const verifiedToken = jwt.verify(id_token, sigingKey);
-    const {
-      nonce: decodedNonce,
-      aud: audience,
-      exp: expirationDate,
-      iss: issuer
-    } = verifiedToken;
-    const currentTime = Math.floor(Date.now() / 1000);
-    const expectedAudience = process.env.CLIENT_ID;
-
-    // TODO: stateの検証やってない
-    if (
-      audience !== expectedAudience ||
-      decodedNonce !== nonce ||
-      expirationDate < currentTime ||
-      issuer !== oidcProviderInfo['issuer']
-    ) {
-      return res.status(401).send();
-    }
-
-    req.session.decodedIdToken = verifiedToken;
-    req.session.idToken = id_token;
-    res.redirect('./profile');
-  });
+  try {
+    req.session.decodedIdToken = validateIDToken(tokens.id_token, nonce);
+    req.session.idToken = tokens.id_token;
+    res.redirect('/profile');
+  } catch (error) {
+    res.status(401).send();
+  }
 });
 
 app.get('/to-dos', async (req, res) => {
-  res.status(501).send();
+  const delegatedRequestOptions = {
+    url: 'http://localhost:3100',
+    headers: {
+      Authorization: `Bearer ${req.session.accessToken}`,
+    },
+  };
+  try {
+    const delegatedResponse = await request(delegatedRequestOptions);
+    const toDos = JSON.parse(delegatedResponse);
+
+    res.render('to-dos', { toDos });
+  } catch (error) {
+    res.status(error.statusCode).send(error);
+  }
 });
 
 app.get('/remove-to-do/:id', async (req, res) => {
@@ -126,13 +160,13 @@ app.get('/remove-to-do/:id', async (req, res) => {
 const { OIDC_PROVIDER } = process.env;
 const discEnd = `https://${OIDC_PROVIDER}/.well-known/openid-configuration`;
 request(discEnd)
-  .then(res => {
+  .then((res) => {
     oidcProviderInfo = JSON.parse(res);
     app.listen(3000, () => {
       console.log(`Server running on http://localhost:3000`);
     });
   })
-  .catch(err => {
+  .catch((err) => {
     console.error(err);
     console.error(`Unable to get OIDC endpoints for ${OIDC_PROVIDER}`);
     process.exit(1);
